ec7fc6205179a291296f41c2bbe51175c5648d67
Louay Cherif <louaycherif182@gmail.com>
2026-05-02 14:42:17 +0100

    week list of words and configs are now gathered in one file called weeks.py instead of separated files and are planned to be imported instead

--- w1words.py
+++
@@ -1,3 +0,0 @@
-# Placeholder word list for Week 1.
-# Populate this list with home-row practice words later.
-words = []
---
+++ weeks.py
@@ -0,0 +1,161 @@
+# words for week 1
+w1words = [
+    "sad", "dad", "fad", "lad", "jam", "ham", "mad", "had", "gas", "lag",
+    "dash", "lash", "gash", "hash", "slam", "sham", "jash", "fash", "galm", "hald",
+    "jams", "lags", "gads", "dams", "shad", "shag", "slag", "flag", "glad", "slam",
+    "flam", "sham", "jamd", "gald", "halm", "jald", "dhal", "gash", "lash", "shaj",
+    "fadh", "dalf", "jalf", "salg", "gafl", "dlag", "jash", "shaf", "lash", "dham"
+]
+# words for week 2
+w2words = [
+    "AIDE", "AIMER", "AILE", "AIR", "AMI", "ARRET", "ARME", "ART", "ASTRE",
+"AUTEUR", "AUTRE", "DAME", "DATE", "DIRE", "DROIT", "DURER", "FAIRE",
+"FAIT", "FIER", "FILE", "FILLE", "FORME", "GARE", "GELER", "GUIDE",
+"HAIE", "HEURE", "HIER", "JETER", "JOLIE", "JOUR", "LAIT", "LAME",
+"LIRE", "LISTE", "LOGER", "LOI", "MAIRE", "MARE", "MARI", "MATIERE",
+"MODE", "MOTEUR", "MUR", "PAR", "PAREIL", "PART", "PARLER", "PAYER",
+"PEUR", "PLUIE", "POIRE", "POIDS", "PORTE", "POSER", "QUAI", "QUALITE",
+"RARE", "RIRE", "ROLE", "ROUE", "SALE", "SAUT", "SEUIL", "SOIF",
+"SOIR", "SOL", "SORT", "SUITE", "TAIRE", "TARD", "TASSE", "TERRE",
+"TITRE", "TOILE", "TOUR", "TRAIT", "UTILISE", "USAGE", "ZESTE",
+"ADIEU", "AFRIQUE", "AGIR", "AIL", "AIME", "AIRE", "ALORS", "AMOUR",
+"APRES", "ARRIERE", "ATOUT", "AURORE", "AUTEL", "AZUR", "DEGAT",
+"DELAI", "DETOUR", "DIEU", "DIGUE", "DIODE", "DROLE",
+"DUEL", "EAU", "EFFET", "EGAL", "ELFE", "ELLE", "EMEU",
+"EMILE", "EMPIRE", "EPAULE", "EPEE", "EPOQUE", "EQUIPE", "ERREUR",
+"ESSAI", "ESSAIM", "ETAGE", "ETOILE", "ETUDE", "EURO", "FALAISE",
+"FAUTEUIL", "FEE", "FETE", "FEU", "FEUTRE", "FIGURE",
+"FILET", "FLEUR", "FOLIE", "FOUET", "FRAISE", "FRERE", "FROMAGE",
+"FUMEE", "GAGE", "GALA", "GARAGE", "GAZ", "GELULE", "GESTE",
+"GLOIRE", "GOMME", "GORGE", "GOUJAT", "GOURDE", "GRILLE", "GUERRE",
+"HARDE", "HAUT", "HORDE", "HORS", "HOTEL", "HUILE",
+"HUMEUR", "JAUGE", "JAZZ", "JOLI", "JOUET", "JUGE", "JULES",
+"JURISTE", "LARGE", "LARME", "LAURIE", "LEGUME", "LEUR", "LIER",
+"LITRE", "LOURD", "LUIRE", "LUMIERE", "MADAME", "MAGIE", "MALADE",
+"MARQUE", "MARTEAU", "MERE", "MESURE", "METAL",
+"MIEL", "MILIEU", "MOI", "MOIS", "MOT", "MOULE", "MOURIR",
+"MUSIQUE", "PARADIS", "PARURE", "PATE", "PATIO", "PAUSE", "PELOTE",
+"PERE", "PERLE", "PETIT", "PHARE", "PIED", "PIERRE", "PILOTE",
+"PLAIRE", "PLEUR", "PLUME", "POLE", "POMPE", "POSTE", "POT", "POTE",
+"POUDRE", "POULE", "POUR", "POURSUITE", "POUSSE", "POUTRE", "PRES",
+"PRET", "PRIERE", "PRISE", "PROIE", "PUIS", "PUR", "PURE", "QUART",
+"QUATRE", "QUE", "QUEL", "QUITTER", "QUOI", "RADIO", "RAIE", 
+"RATE", "REALITE", "REDUIRE", "REFUS", "REGARD", "REGLE", 
+"REJET", "RELAIS", "REPAS", "REPERE", "REPOS", "RESEAU",
+"RESTE", "RETARD", "REUSSIR", "RIGOLE", "RIME", "RISQUE", "RITE",
+"RODE", "ROI", "ROSE", "ROUGE", "ROULE", "RUE",
+"RUMEUR", "SAGE", "SAISIR", "SALADE", "SALIR", "SALUT",
+"SEL", "SERIE", "SERRE", "SEUL", "SIEGE",
+"SIESTE", "SITOT", "SITUER", "SOEUR", "SOLEIL", "SOLIDE", "SOLO",
+"SORTIE", "SOUFFLE", "SOUHAIT", "SOUPLE", "SOURIRE",
+"SOUS", "SOUTE", "SUD", "SUER", "SUJET", "SUPER", "SUR",
+"SURETE", "SURGI", "SURPRISE", "TARTE", "TAS",
+"TASSER", "TAUDIS", "TAUPE", "TEMPLE", "TEST",
+"TETE", "TISSU", "TOILETTE", "TOIT", "TOURTE", "TOUT", 
+"TRAITE", "TRAME", "TRAPEZE", "TRAQUE", 
+"TREFLE", "TREIZE", "TREMPER", "TRES", "TRESOR", "TRETEAU", "TRI",
+"TRIER", "TRIO", "TRISTE", "TROIS", "TROMPE", "TROP", "TROU",
+"TROUPE", "TUER", "TUILERIE", "TUMEUR", "TUTELAIRE", "TUTTI", "TYPE",
+"TYPIQUE", "USE", "USURE", "UTILE", "UTOPIE", "YAOURT", "YOGOURT"
+]
+# words for week 3
+w3words = [
+    "chat","chien","maison","table","porte","fenetre","route","ville","arbre","plage",
+"soleil","lune","etoile","ciel","terre","mer","vent","pluie","neige","orage",
+"feu","eau","pain","fromage","lait","sucre","sel","poivre","riz","pate",
+"fruit","pomme","poire","peche","banane","orange","citron","fraise","raisin","melon",
+"legume","carotte","tomate","salade","oignon","ail","haricot","pomme","terreau","radis",
+"ecole","classe","prof","eleve","livre","stylo","cahier","tableau","cours","devoir",
+"bureau","chaise","ordinateur","clavier","souris","ecran","fenetre","porte","mur","plafond",
+"jardin","fleur","herbe","arbre","branche","racine","feuille","fruitier","rose","tulipe",
+"animal","cheval","vache","mouton","chevre","poule","canard","lapin","singe","tigre",
+"lion","ours","loup","renard","cerf","biche","poisson","requin","baleine","dauphin",
+"voiture","train","avion","velo","moto","bus","route","gare","aeroport","station",
+"hotel","chambre","lit","drap","oreiller","couverture","serviette","douche","bain","savon",
+"travail","metier","emploi","salaire","bureau","chef","collegue","reunion","projet","rapport",
+"temps","heure","minute","seconde","jour","semaine","mois","annee","matin","soir",
+"amour","ami","famille","pere","mere","frere","soeur","enfant","voisin","relation",
+"musique","chanson","danse","film","theatre","livre","histoire","conte","roman","poeme"
+]
+# week 1 structure
+week1 = {
+                "name": "Semaine 1: Ligne de Base",
+                "steps": ["QSDF", "GHJ", "KLM", "QSDFGHJKLM"],
+                "practice_letters": ["QSDFGHJKLM"],
+                "words": w1words,
+                "learning_flow": [
+                    {"mode": "learn", "chars": "QSDF"},
+                    {"mode": "random", "chars": "QSDF", "duration": 300},
+                    {"mode": "learn", "chars": "GHJ"},
+                    {"mode": "random", "chars": "GHJ", "count": 40},
+                    {"mode": "random", "chars": "QSDFGHJ", "duration": 300},
+                    {"mode": "learn", "chars": "KLM"},
+                    {"mode": "random", "chars": "KLM", "count": 40},
+                    {"mode": "random", "chars": "QSDFGHJKLM", "count": 250},
+                ],
+            }
+# week 2 structure
+week2 = {
+                "name": "Semaine 2: Ligne Supérieure",
+                        "steps": ["AZER", "TYU", "IOP", "AZERTYUIOP"],
+                "letters": "AZERTYUIOP",
+                "practice_letters": ["AZERTYUIOP", "AZERTYUIOPQSDFGHJKLM"],
+                "words": w2words,
+                "learning_flow": [
+                    {"mode": "random", "chars": "QSDFGHJKLM", "duration": 300},
+                    {"mode": "learn", "chars": "AZER"},
+                    {"mode": "random", "chars": "AZER", "count": 20},
+                    {"mode": "learn", "chars": "TYU"},
+                    {"mode": "random", "chars": "TYU", "count": 20},
+                    {"mode": "learn", "chars": "IOP"},
+                    {"mode": "random", "chars": "IOP", "count": 20},
+                    {"mode": "random", "chars": "AZERTYUIOP", "count": 40},
+                    {"mode": "random", "chars": "AZERTYUIOPQSDFGHJKLM", "count": 60},
+                ],
+            }
+# week 3 structure
+week3 = {
+                "name": "Semaine 3: Ligne Inférieure",
+                "steps": ["WXC", "VBN", "WXCVBN"],
+                "letters": "WXCVBN",
+                "practice_letters": ["WXCVBN", "AZERTYUIOPQSDFGHJKLMWXCVBN"],
+                "words": w3words,
+                "learning_flow": [
+                    {"mode": "random", "chars": "QSDFGHJKLM", "duration": 300},
+                    {"mode": "random", "chars": "AZERTYUIOP", "duration": 300},
+                    {"mode": "random", "chars": "AZERTYUIOPQSDFGHJKLM", "count": 100},
+                    {"mode": "learn", "chars": "WXC"},
+                    {"mode": "random", "chars": "WXC", "count": 20},
+                    {"mode": "random", "chars": "AZERTYUIOPWXC", "duration": 180},
+                    {"mode": "learn", "chars": "VBN"},
+                    {"mode": "random", "chars": "VBN", "duration": 120},
+                    {"mode": "random", "chars": "WXCVBN", "duration": 180},
+                    {"mode": "random", "chars": "AZERTYUIOPQSDFGHJKLMWXCVBN", "duration": 600},
+                ],
+            }
+
+# week 4 structure
+week4 = {
+    "name": "Semaine 4: Mixage des Lignes",
+    "steps": ["QSDFGHJKLM", "AZERTYUIOP", "WXCVBN", "AZERTYUIOPQSDFGHJKLMWXCVBN"],
+    "practice_letters": ["QSDFGHJKLM", "AZERTYUIOP", "WXCVBN", "AZERTYUIOPQSDFGHJKLMWXCVBN"],
+    "words": [],
+    "learning_flow": [
+        {"mode": "random", "chars": "QSDFGHJKLM", "duration": 180},
+        {"mode": "random", "chars": "AZERTYUIOP", "duration": 180},
+        {"mode": "random", "chars": "WXCVBN", "duration": 180},
+        {"mode": "random", "chars": "AZERTYUIOPQSDFGHJKLMWXCVBN", "duration": 300},
+    ],
+}
+
+# week 5 structure
+week5 = {
+    "name": "Semaine 5: Majuscules et Symboles",
+    "steps": ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", ",;:!?", "./§", "ALL_COMBINED"],
+    "practice_letters": ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
+    "words": [],
+    "learning_flow": [
+        {"mode": "learn", "chars": "ABCDEFGHIJKLMNOPQRSTUVWXYZ"},
+        {"mode": "random", "chars": "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "duration": 300},
+    ],
+}
